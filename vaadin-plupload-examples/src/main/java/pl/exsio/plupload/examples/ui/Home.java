a14497af9f9267c5db5c46c01e70d12275cbd790
eXsio <sdymitrow@gmail.com>
2015-01-14 10:20:56 +0100

    pom upd

--- vaadin-plupload-examples/src/main/java/pl/exsio/plupload/examples/ui/Home.java
+++ vaadin-plupload-examples/src/main/java/pl/exsio/plupload/examples/ui/Home.java
@@ -16,7 +16,7 @@ public class Home extends VerticalLayout {
                 + "Here are a couple of things, that you should know about:</p><ul>"
                 + "<li>For the sake of performance and security the uploaded <b>file size</b> in all examples is restricted to <b>5MB</b></li>"
                 + "<li>After the upload is completed, files are <b>immediately deleted</b></li>"
-                + "<li>Despite the above, your own comfort we recommend that You <b>don't upload</b> files containing <b>fragile/secret informations</b></li>"
+                + "<li>Despite the above, for your own comfort it is recommended, that You <b>don't upload</b> files containing <b>fragile/secret informations</b></li>"
                 + "<li><b>PluploadManager</b> and <b>PluploadField</b> are 100% server-side components created around the basic <b>Plupload</b> component</li>"
                 + "<li>Source code of the add-on is available <a target=\"_blank\" href=\"https://github.com/eXsio/vaadin-plupload\">here</a></li>"
                 + "<li>Source code of the examples app is available <a target=\"_blank\" href=\"https://github.com/eXsio/vaadin-plupload-examples\">here</a></li>"
